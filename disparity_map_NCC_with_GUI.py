835860bb11a09b23313bd8dff870ccccc40b7730
wyoung1810 <ukjoma@gmail.com>
2024-11-06 09:37:18 -0500

    implemented pixel correlation alternatives and GUIs

--- disparity_map_NCC_with_GUI.py
+++ disparity_map_NCC_with_GUI.py
@@ -1,14 +1,15 @@
 import numpy as np
 import cv2 as cv
+import copy
 import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use('TKAgg')
 
 # Helper Function
-def calculate_SSD(matrix1, matrix2): # use argmin for finding best fit
+def calculate_SSD(matrix1, matrix2):
     return np.sum((matrix1 - matrix2) ** 2, axis=(-2, -1))
 
-def calculate_NCC(matrix1, matrix2): # use argmax for finding best fit
+def calculate_NCC(matrix1, matrix2):
     # Ensure matrices are numpy arrays for compatibility
     matrix1 = np.asarray(matrix1)
     matrix2 = np.asarray(matrix2)
@@ -29,8 +30,8 @@ def calculate_NCC(matrix1, matrix2): # use argmax for finding best fit
     return np.where(denominator != 0, numerator / denominator, 0)
 
 # Import images into CV
-img_left = cv.imread("./test_images/view1.png")
-img_right = cv.imread("./test_images/view5.png")
+img_left = cv.imread("./test_images/bview1.png")
+img_right = cv.imread("./test_images/bview5.png")
 
 # Create grayscale versions of image
 img_left_gray = cv.cvtColor(img_left, cv.COLOR_BGR2GRAY)
@@ -60,36 +61,55 @@ for y in range(0, img_height - frame_thickness * 2):
     # Calculate SSDs for the whole row in the right image at once
     left_kernels = left_pixel_kernel_matrix[y]
     # print(left_kernels.shape)
-    # SSD for each pixel's kernel against all kernels in the same row of the right image
-    ssd = calculate_SSD(left_kernels[:, np.newaxis], right_pixel_kernel_matrix[y,:])
-    print(ssd)
+    # NCC for each pixel's kernel against all kernels in the same row of the right image
+    ncc = calculate_NCC(left_kernels[:, np.newaxis], right_pixel_kernel_matrix[y,:])
+    # print(NCC)
     # Find the best matching pixel for each pixel in the left image
-    best_matching_pixel_xcoord = np.argmin(ssd, axis=1)
+    best_matching_pixel_xcoord = np.argmax(ncc, axis=1)
     # print(np.abs(best_matching_pixel_xcoord - np.arange(frame_thickness, img_width - frame_thickness)))
     disparity_map[y + frame_thickness, frame_thickness:img_width - frame_thickness] = np.abs(best_matching_pixel_xcoord - np.arange(frame_thickness, img_width - frame_thickness))
     # print(disparity_map)
     print(y / (img_height - 2 * frame_thickness) * 100)  # Loading bar
 
 
-# for i in range(1, img_height - 1):
-#     for j in range(2, img_width - 2):
-#         if disparity_map[i,j] > 75:
-#             disparity_map[i,j] = 0
+# Implement the GUI
+threshold = 255
+blur_size = 1
 
-threshold = 75
-disparity_map[disparity_map > threshold] = 0
+def nothing(x):
+    pass
 
+cv.namedWindow('disp',cv.WINDOW_NORMAL)
+cv.resizeWindow('disp',600,600)
 
-disparity_map_scaled = cv.normalize(disparity_map, None, 0, 255, cv.NORM_MINMAX)  # Normalize to 0-255
-disparity_map_uint8 = np.uint8(disparity_map_scaled)  # Convert to uint8
-blurred_disparity_map = cv.medianBlur(disparity_map_uint8, 5)
-# blurred_disparity_map = cv.GaussianBlur(disparity_map_uint8, (5,5), 1)
+cv.createTrackbar('threshold', 'disp', 0, 255, nothing)
+cv.createTrackbar('blur size', 'disp', 1, 20, nothing)
+ 
+while True:
+    # Updating the parameters based on the trackbar positions
+    
+    threshold = cv.getTrackbarPos('threshold','disp')
+    blur_size = cv.getTrackbarPos('blur size', 'disp') * 2 - 1
+    
+    # Calculating stuff
+    disparity_map_gui = copy.deepcopy(disparity_map)
 
-fig, ax = plt.subplots(nrows=1, ncols=3)
-ax[0].imshow(disparity_map_uint8, cmap='gray')
-ax[1].imshow(img_left_gray, cmap='gray')
-ax[2].imshow(img_right_gray, cmap='gray')
-plt.show()
+    for i in range(1, img_height - 1):
+        for j in range(2, img_width - 2):
+            if disparity_map[i,j] > threshold:
+                disparity_map_gui[i,j] = 0
 
-print(disparity_map)
-print(disparity_map[216,270])
+    disparity_map_scaled = cv.normalize(disparity_map_gui, None, 0, 255, cv.NORM_MINMAX)  # Normalize to 0-255
+    disparity_map_uint8 = np.uint8(disparity_map_scaled)  # Convert to uint8
+
+    try: 
+        blurred_disparity_map = cv.medianBlur(disparity_map_uint8, blur_size)
+    except: 
+        blurred_disparity_map = cv.medianBlur(disparity_map_uint8, 1)
+
+    # Displaying the disparity map
+    cv.imshow("disp",blurred_disparity_map)
+
+    # Close window using esc key
+    if cv.waitKey(1) == 27:
+        break    
